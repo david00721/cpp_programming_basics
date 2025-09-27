89a04c08e3a481d519cca69a97eee225321a6096
david007214 <david007214@gmail.com>
2025-09-27 10:57:37 +0200

    lesson 2 solutions fixed

--- solutions/curriculum/02_solutions/02.cpp
+++ solutions/curriculum/02_solutions/02.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    // Írj programot, ami bekér egy számot, és kiírja, hogy nagyobb-e 10-nél!  
+    // 01 -Írj programot, ami bekér egy számot, és kiírja, hogy nagyobb-e 10-nél!  
     int number;
     cout << "Adj meg egy szamot: ";
     cin >> number;
@@ -14,7 +14,7 @@ int main()
         cout << "A szam nem nagyobb mint 10." << endl;
     }
 
-    // Írj programot, ami bekér egy számot, és kiírja, hogy kisebb-e 100-nál!
+    // 02 - Írj programot, ami bekér egy számot, és kiírja, hogy kisebb-e 100-nál!
     cout << "Adj meg egy szamot: ";
     cin >> number;
     if (number < 100) {
@@ -23,7 +23,7 @@ int main()
         cout << "A szam nem kisebb mint 100." << endl;
     }
 
-    // Írj programot, ami bekér egy számot, és kiírja, hogy pozitív vagy nem!  
+    // 03 - Írj programot, ami bekér egy számot, és kiírja, hogy pozitív vagy nem!  
     cout << "Adj meg egy szamot: ";
     cin >> number;
     if (number > 0) {
@@ -32,7 +32,7 @@ int main()
         cout << "A szam nem pozitiv." << endl;
     }
 
-    // Írj programot, ami bekér egy számot, és kiírja, hogy páros vagy páratlan!
+    // 04 - Írj programot, ami bekér egy számot, és kiírja, hogy páros vagy páratlan!
     cout << "Adj meg egy szamot: ";
     cin >> number;
     if (number % 2 == 0) {
@@ -41,7 +41,7 @@ int main()
         cout << "A szam paratlan." << endl;
     }
 
-    // Írj programot, ami bekér egy számot, és kiírja, hogy pozitív, negatív vagy nulla!  
+    // 05 - Írj programot, ami bekér egy számot, és kiírja, hogy pozitív, negatív vagy nulla!
     cout << "Adj meg egy szamot: ";
     cin >> number;
     if (number > 0) {
@@ -52,7 +52,7 @@ int main()
         cout << "A szam nulla." << endl;
     }
 
-    //Írj programot, ami bekér egy számot, és eldönti róla, hogy osztható-e 3-mal, 5-tel vagy mindkettővel!
+    // 06 - Írj programot, ami bekér egy számot, és eldönti róla, hogy osztható-e 3-mal, 5-tel vagy mindkettővel!
     cout << "Adj meg egy szamot: ";
     cin >> number;
     if (number % 3 == 0 && number % 5 == 0) {
@@ -65,7 +65,7 @@ int main()
         cout << "A szam nem oszthato sem 3-mal, sem 5-tel." << endl;
     }
 
-    // Írj programot, ami bekér egy jegyet (A–F), és szöveges értékelést ad rá!
+    // 07 - Írj programot, ami bekér egy jegyet (A–F), és szöveges értékelést ad rá!
     char grade;
     cout << "Adj meg egy jegyet (A-F): ";
     cin >> grade;
@@ -92,7 +92,7 @@ int main()
             cout << "Érvénytelen jegy!" << endl;
     }
 
-    // Írj programot, ami bekér egy számot 1–7 között, és kiírja a hét megfelelő napját!
+    // 08 - Írj programot, ami bekér egy számot 1–7 között, és kiírja a hét megfelelő napját!
     int day;
     cout << "Adj meg egy szamot 1-7 kozott: ";
     cin >> day;
@@ -122,13 +122,13 @@ int main()
             cout << "Érvénytelen szám!" << endl;
     }
 
-    // Írj programot, ami kiírja az 1–10 közötti számokat!
+    // 09 - Írj programot, ami kiírja az 1–10 közötti számokat!
     for (int i = 1; i <= 10; i++) {
         cout << i << " ";
     }
     cout << endl;
 
-    // Írj programot, ami kiírja az 1–20 közötti páros számokat!
+    // 10 - Írj programot, ami kiírja az 1–20 közötti páros számokat!
     for (int i = 1; i <= 20; i++) {
         if (i % 2 == 0) {
             cout << i << " ";
@@ -136,27 +136,27 @@ int main()
     }
     cout << endl;
 
-    // Írj programot, ami kiírja egy tömb minden elemét!
+    // 11 - Írj programot, ami kiírja egy tömb minden elemét!
     int arr[] = {1, 2, 3, 4, 5};
     for (int i = 0; i < 5; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
 
-    // Írj programot, ami összeadja egy tömb elemeit!
+    // 12 - Írj programot, ami összeadja egy tömb elemeit!
     int sum = 0;
     for (int i = 0; i < 5; i++) {
         sum += arr[i];
     }
     cout << "A tomb elemeinek osszege: " << sum << endl;
 
-    // Írj programot, ami kiírja a tömb elemeit range-based for ciklussal!
+    // 13 - Írj programot, ami kiírja a tömb elemeit range-based for ciklussal!
     for (const auto& elem : arr) {
         cout << elem << " ";
     }
     cout << endl;
 
-    // Írj programot, ami megszámolja, hány páros szám van a tömbben!
+    // 14 - Írj programot, ami megszámolja, hány páros szám van a tömbben!
     int count = 0;
     for (const auto& elem : arr) {
         if (elem % 2 == 0) {
@@ -165,7 +165,7 @@ int main()
     }
     cout << "Paros szamok szama a tombben: " << count << endl;
 
-    // Írj programot, ami while ciklussal kiírja az 1–10 közötti számokat!
+    // 15 - Írj programot, ami while ciklussal kiírja az 1–10 közötti számokat!
     cout << "1-10 kozotti szamok: ";
     int i = 1;
     while (i <= 10) {
@@ -174,7 +174,7 @@ int main()
     }
     cout << endl;
 
-    // Írj programot, ami while ciklussal összeadja az 1–n közötti számokat!
+    // 16 - Írj programot, ami while ciklussal összeadja az 1–n közötti számokat!
     int n;
     cout << "Adj meg egy szamot n: ";
     cin >> n;
@@ -187,7 +187,7 @@ int main()
     cout << "Az 1-tol " << n << "ig terjedo szamok osszege: " << total << endl;
 
 
-    // Írj programot, ami do-while ciklussal legalább egyszer kiír egy üzenetet!
+    // 17 - Írj programot, ami do-while ciklussal legalább egyszer kiír egy üzenetet!
     string message;
     do {
         cout << "Adj meg egy uzenetet (kilepeshez irj 'exit'): ";
@@ -197,7 +197,7 @@ int main()
         }
     } while (message != "exit");
 
-    // Írj programot, ami do-while ciklussal addig kér be számot, amíg a felhasználó nullát nem ad meg!
+    // 18 - Írj programot, ami do-while ciklussal addig kér be számot, amíg a felhasználó nullát nem ad meg!
     int num;
     do {
         cout << "Adj meg egy szamot (kilepeshez irj 0): ";
@@ -207,7 +207,7 @@ int main()
         }
     } while (num != 0);
 
-    // Írj programot, ami 1–10 között kiír minden számot, kivéve az 5-öst!
+    // 19 - Írj programot, ami 1–10 között kiír minden számot, kivéve az 5-öst!
     cout << "1-10 kozotti szamok (5-os nelkul): ";
     for (int i = 1; i <= 10; i++) {
         if (i == 5) {
@@ -217,7 +217,7 @@ int main()
     }
     cout << endl;
 
-    // Írj programot, ami 1–20 között kiírja a számokat, de ha 13-hoz ér, kilép a ciklusból!
+    // 20 - Írj programot, ami 1–20 között kiírja a számokat, de ha 13-hoz ér, kilép a ciklusból!
     cout << "1-20 kozotti szamok (13-nal kilepes): ";
     for (int i = 1; i <= 20; i++) {
         if (i == 13) {
@@ -227,7 +227,7 @@ int main()
     }
     cout << endl;
 
-    // Írj programot, ami beágyazott ciklussal kirajzol egy 5×5-ös négyzetet `*` karakterekből!
+    // 21 - Írj programot, ami beágyazott ciklussal kirajzol egy 5×5-ös négyzetet `*` karakterekből!
     cout << "5x5-os negyzet '*' karakterekbol:" << endl;
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
@@ -236,7 +236,7 @@ int main()
         cout << endl;
     }
 
-    // Írj programot, ami beágyazott ciklussal kirajzol egy derékszögű háromszöget `*` karakterekből!
+    // 22 - Írj programot, ami beágyazott ciklussal kirajzol egy derékszögű háromszöget `*` karakterekből!
     cout << "Derekszogu haromszog '*' karakterekbol:" << endl;
     for (int i = 1; i <= 5; i++) {
         for (int j = 1; j <= i; j++) {
@@ -245,7 +245,7 @@ int main()
         cout << endl;
     }
 
-    // Írj programot, ami kiírja 1-től 100-ig az összes páros számot!
+    // Gyak 01 - Írj programot, ami kiírja 1-től 100-ig az összes páros számot!
     cout << "1-100 kozotti paros szamok: ";
     for (int i = 1; i <= 100; i++) {
         if (i % 2 == 0) {
@@ -254,7 +254,7 @@ int main()
     }
     cout << endl;
 
-    // Írj programot, ami kiszámolja az első n természetes szám összegét (1+2+…+n)!
+    // Gyak 02 - Írj programot, ami kiszámolja az első n természetes szám összegét (1+2+…+n)!
     cout << "Adj meg egy szamot n: ";
     cin >> n;
     total = 0;
@@ -263,7 +263,7 @@ int main()
     }
     cout << "Az elso " << n << " termeszetes szam osszege: " << total << endl;
 
-    // Kérj be egy számot, és döntsd el róla, hogy prímszám-e!
+    // Gyak 03 - Kérj be egy számot, és döntsd el róla, hogy prímszám-e!
     cout << "Adj meg egy szamot: ";
     cin >> number;
     bool isPrime = true;
@@ -284,7 +284,7 @@ int main()
         cout << number << " nem primszam." << endl;
     }
 
-    // Írj programot, ami kirajzol egy 10×10-es négyzetet `*` karakterekből (csak a körvonalát)!
+    // Gyak 04 - Írj programot, ami kirajzol egy 10×10-es négyzetet `*` karakterekből (csak a körvonalát)!
     const int n = 10;
 
     cout << "10x10-es negyzet '*' karakterekbol (csak a korvonal):" << endl;
@@ -299,7 +299,7 @@ int main()
         cout << '\n';
     }
 
-    // Írj egyszerű menüt, ami addig ismétlődik, amíg a felhasználó ki nem lép!
+    // Gyak 05 - Írj egyszerű menüt, ami addig ismétlődik, amíg a felhasználó ki nem lép!
     int choice;
     do {
         cout << "Menu:" << endl;
